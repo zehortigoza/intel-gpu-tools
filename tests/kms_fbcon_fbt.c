zehortigoza/intel-gpu-tools

commit 9c4114ec5d8700815eb1f400b99add4f26070568

lib: Always unbind the fbcon around igt
The fbcon imposes unpredictable latencies on tests - each drmIoctl has
been observed to trigger two 650us calls to console_unlock() as it
flushes printk buffer for the DRM_DEBUG around the ioctl. This makes
tests such as gem_wait fail as they expect the ioctl to be spent on the
operation under test not clogged up by the console.

References: https://bugs.freedesktop.org/show_bug.cgi?id=99130
Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

lib/igt_core.c
@@ -67,6 +67,7 @@
 
 #include "igt_core.h"
 #include "igt_aux.h"
+#include "igt_sysfs.h"
 
 #ifdef HAVE_LIBGEN_H
 #include <libgen.h>   /* for basename() on Solaris */
@@ -512,6 +513,7 @@ static void common_exit_handler(int sig)
 {
 	if (!igt_only_list_subtests()) {
 		low_mem_killer_disable(false);
+		kick_fbcon(true);
 	}
 
 	/* When not killed by a signal check that igt_exit() has been properly
@@ -731,6 +733,7 @@ out:
 		exit(ret == -1 ? 0 : IGT_EXIT_INVALID);
 
 	if (!list_subtests) {
+		kick_fbcon(false);
 		kmsg(KERN_INFO "[IGT] %s: executing\n", command_str);
 		print_version();
 

commit f71652b9bd590c30b2f847d44ecc43248e5547fb

tests/fbcon_fbt: Allow fbcon to bind when running this test
9c4114ec5d87 (lib: Always unbind the fbcon around igt) broke
fbcon_fbt test as fbcon is not allowd to bind when executing any IGT
test, so lets allow it again just for this test.

Cc: Paulo Zanoni <paulo.r.zanoni@intel.com>
Cc: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
Cc: Chris Wilson <chris@chris-wilson.co.uk>
Signed-off-by: José Roberto de Souza <jose.souza@intel.com>

tests/kms_fbcon_fbt.c
@@ -26,6 +26,7 @@
 
 #include "igt.h"
 #include "igt_psr.h"
+#include "igt_sysfs.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -274,6 +275,12 @@ static void setup_environment(void)
 	drm_fd = drm_open_driver_master(DRIVER_INTEL);
 	igt_require(drm_fd >= 0);
 	igt_assert(close(drm_fd) == 0);
+
+	/*
+	 * igt_subtest_init_parse_opts() disable the fbcon bind, so to test it
+	 * is necessary enable it again
+	 */
+	bind_fbcon(true);
 }
 
 static void teardown_environment(void)