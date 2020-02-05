d4c7f3f06f7aaba8c16b4fdd43866074a6f13ae3
José Roberto de Souza <jose.souza@intel.com>
2020-02-05 14:45:04 -0800

    Revert "Revert "tests/kms_frontbuffer_tracking: Enable positive test on linear tiling""
    This reverts commit f5391e3bf85bb98920ef65910df412eb67d4ae64.

--- tests/kms_frontbuffer_tracking.c
+++ tests/kms_frontbuffer_tracking.c
@@ -2163,7 +2163,7 @@ static bool tiling_is_valid(int feature_flags, enum tile_type tile)
 {
 	int devid = intel_get_drm_devid(drm.fd);
 
-	if (tile == TILE_LINEAR)
+	if (tile == TILE_LINEAR && !AT_LEAST_GEN(devid, 11))
 		return false;
 
 	return true;
