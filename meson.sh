4dfe9664f8585f70773ad7836e25b0b6eb6a9e75
Mauro Carvalho Chehab <mchehab@kernel.org>
2023-12-11 12:37:09 +0100

    meson.sh: allow making it less verbose
    Add support for not output each build step and run ninja
    with --quiet option.
    
    With this patch, meson.sh now supports:
    
    	./meson.sh V=0
    
    which will disable non-error messages while running
    ninja build.
    
    This is interesting specially when executed inside some
    script.
    
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- meson.sh
+++ meson.sh
@@ -2,36 +2,50 @@
 
 cat > Makefile <<EOF
 
+quiet_build =
+quiet_reconfigure =
+Q =
+VERBOSE =
+
+ifeq ("\$(origin V)", "command line")
+  VERBOSE = \$(V)
+endif
+
+ifneq (\$(findstring 0, \$(VERBOSE)),)
+  quiet_build = --quiet
+  Q = @
+endif
+
 .PHONY: default docs
 default: all
 
 build/build.ninja:
-	mkdir -p build
-	meson build
+	\$(Q)mkdir -p build
+	\$(Q)meson build \$(quiet_build)
 
 all: build/build.ninja
-	ninja -C build
+	\$(Q)ninja -C build \$(quiet_build)
 
 clean: build/build.ninja
-	ninja -C build clean
+	\$(Q)ninja -C build clean \$(quiet_build)
 
 test: build/build.ninja
-	ninja -C build test
+	\$(Q)ninja -C build test \$(quiet_build)
 
 reconfigure: build/build.ninja
-	ninja -C build reconfigure
+	\$(Q)ninja -C build reconfigure
 
 check distcheck dist distclean:
-	echo "This is the meson wrapper, not automake" && false
+	@echo "This is the meson wrapper, not automake" && false
 
 install: build/build.ninja
-	ninja -C build install
+	\$(Q)ninja -C build install \$(quiet_build)
 
 uninstall: build/build.ninja
-	ninja -C build uninstall
+	\$(Q)ninja -C build uninstall \$(quiet_build)
 
 docs:
-	ninja -C build igt-gpu-tools-doc
+	\$(Q)ninja -C build igt-gpu-tools-doc \$(quiet_build)
 
 EOF
 
