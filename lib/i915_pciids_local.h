2731d208257db9f5df384bfc58453c312ea77765
Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
2024-01-10 13:45:31 +0530

    lib: Add non-kernel macros to i915_pciids_local.h
    Add MTL and PVC PCI-ID macros which are not present in kernel
    header to i915_pciids_local.h. This will help us sync the kernel
    and IGT headers.
    
    v2: fix spaces/tabs, sorted PVC ids (Kamil)
    
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Cc: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
    Cc: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Cc: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: Niranjana Vishwanathapura <niranjana.vishwanathapura@intel.com>
    Reviewed-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

--- lib/i915_pciids_local.h
+++ lib/i915_pciids_local.h
@@ -7,4 +7,42 @@
 
 #include "i915_pciids.h"
 
+/* MTL perf */
+#ifndef INTEL_MTL_M_IDS
+#define INTEL_MTL_M_IDS(info) \
+	INTEL_VGA_DEVICE(0x7D60, info)
+#endif
+
+#ifndef INTEL_MTL_P_GT2_IDS
+#define INTEL_MTL_P_GT2_IDS(info) \
+	INTEL_VGA_DEVICE(0x7D45, info)
+#endif
+
+#ifndef INTEL_MTL_P_GT3_IDS
+#define INTEL_MTL_P_GT3_IDS(info) \
+	INTEL_VGA_DEVICE(0x7D55, info), \
+	INTEL_VGA_DEVICE(0x7DD5, info)
+#endif
+
+#ifndef INTEL_MTL_P_IDS
+#define INTEL_MTL_P_IDS(info) \
+	INTEL_MTL_P_GT2_IDS(info), \
+	INTEL_MTL_P_GT3_IDS(info)
+#endif
+
+/* PVC */
+#ifndef INTEL_PVC_IDS
+#define INTEL_PVC_IDS(info) \
+	INTEL_VGA_DEVICE(0x0BD0, info), \
+	INTEL_VGA_DEVICE(0x0BD1, info), \
+	INTEL_VGA_DEVICE(0x0BD2, info), \
+	INTEL_VGA_DEVICE(0x0BD5, info), \
+	INTEL_VGA_DEVICE(0x0BD6, info), \
+	INTEL_VGA_DEVICE(0x0BD7, info), \
+	INTEL_VGA_DEVICE(0x0BD8, info), \
+	INTEL_VGA_DEVICE(0x0BD9, info), \
+	INTEL_VGA_DEVICE(0x0BDA, info), \
+	INTEL_VGA_DEVICE(0x0BDB, info)
+#endif
+
 #endif /* _I915_PCIIDS_LOCAL_H */
