f67fadec3e2715adf554495c6cad4586d45c7e9f
Ashutosh Dixit <ashutosh.dixit@intel.com>
2024-01-23 13:35:13 -0800

    xe/oa: Cleanup
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

--- lib/xe/xe_oa.c
+++ lib/xe/xe_oa.c
@@ -1,36 +1,31 @@
 // SPDX-License-Identifier: MIT
 /*
- * Copyright © 2023 Intel Corporation
+ * Copyright © 2024 Intel Corporation
  */
 
 #include <assert.h>
+#include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
-#include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
-#ifdef __linux__
 #include <sys/sysmacros.h>
-#endif
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <i915_drm.h>
-
 #include "drmtest.h"
-#include "igt_aux.h"
 #include "i915_pciids.h"
 #include "i915_pciids_local.h"
 #include "intel_chipset.h"
+#include "intel_hwconfig_types.h"
 #include "ioctl_wrappers.h"
 #include "linux_scaffold.h"
 #include "xe_ioctl.h"
 #include "xe_oa.h"
-#include "xe/xe_query.h"
-#include "intel_hwconfig_types.h"
+#include "xe_query.h"
 
 #include "xe_oa_metrics_adl.h"
 #include "xe_oa_metrics_acmgt1.h"
@@ -39,19 +34,6 @@
 #include "xe_oa_metrics_mtlgt2.h"
 #include "xe_oa_metrics_mtlgt3.h"
 #include "xe_oa_metrics_lnl.h"
-
-#define xe_relax_checks(drm_fd) (IS_LUNARLAKE(xe_dev_id(drm_fd)))
-
-static int
-perf_ioctl(int fd, unsigned long request, void *arg)
-{
-	int ret;
-
-	do {
-		ret = ioctl(fd, request, arg);
-	} while (ret == -1 && (errno == EINTR || errno == EAGAIN));
-	return ret;
-}
 
 static struct intel_perf_logical_counter_group *
 intel_perf_logical_counter_group_new(struct intel_perf *perf,
@@ -274,10 +256,7 @@ intel_perf_for_devinfo(uint32_t device_id,
 	       &topology->data[topology->eu_offset],
 	       eu_mask_len);
 
-	/* On Gen11+ the equations from the xml files expect an 8bits
-	 * mask per subslice, versus only 3bits on prior Gens.
-	 */
-	bits_per_subslice = devinfo->graphics_ver >= 11 ? 8 : 3;
+	bits_per_subslice = 8;
 	for (uint32_t s = 0; s < topology->max_slices; s++) {
 		if (!slice_available(topology, s))
 			continue;
@@ -358,18 +337,6 @@ intel_perf_for_devinfo(uint32_t device_id,
 	return perf;
 }
 
-static int
-getparam(int drm_fd, uint32_t param, uint32_t *val)
-{
-	struct drm_i915_getparam gp;
-
-	memset(&gp, 0, sizeof(gp));
-	gp.param = param;
-	gp.value = (int *)val;
-
-	return perf_ioctl(drm_fd, DRM_IOCTL_I915_GETPARAM, &gp);
-}
-
 static bool
 read_fd_uint64(int fd, uint64_t *out_value)
 {
@@ -399,41 +366,6 @@ read_sysfs(int sysfs_dir_fd, const char *file_path, uint64_t *out_value)
 	close(fd);
 
 	return res;
-}
-
-static int
-query_items(int drm_fd, struct drm_i915_query_item *items, uint32_t n_items)
-{
-	struct drm_i915_query q = {
-		.num_items = n_items,
-		.items_ptr = (uintptr_t) items,
-	};
-
-	return perf_ioctl(drm_fd, DRM_IOCTL_I915_QUERY, &q);
-}
-
-static struct drm_i915_query_topology_info *
-query_topology(int drm_fd)
-{
-	struct drm_i915_query_item item;
-	struct drm_i915_query_topology_info *topo_info;
-	int ret;
-
-	memset(&item, 0, sizeof(item));
-	item.query_id = DRM_I915_QUERY_TOPOLOGY_INFO;
-	ret = query_items(drm_fd, &item, 1);
-	if (ret < 0 || item.length < 0)
-		return NULL;
-
-	topo_info = calloc(1, item.length);
-	item.data_ptr = (uintptr_t) topo_info;
-	ret = query_items(drm_fd, &item, 1);
-	if (ret < 0 || item.length < 0) {
-		free(topo_info);
-		return NULL;
-	}
-
-	return topo_info;
 }
 
 static int
@@ -486,33 +418,6 @@ typedef enum {
 
 	RPS_MAX_ATTR,
 } intel_sysfs_attr_id;
-
-static const char *intel_sysfs_attr_name[][RPS_MAX_ATTR] =
-{
-	{
-		"gt_min_freq_mhz",
-		"gt_max_freq_mhz",
-	},
-	{
-		"gt/gt0/rps_min_freq_mhz",
-		"gt/gt0/rps_max_freq_mhz",
-	},
-	{
-		"gt/gt1/rps_min_freq_mhz",
-		"gt/gt1/rps_max_freq_mhz",
-	},
-};
-
-static const char *
-intel_sysfs_attr_id_to_name(int sysfs_dirfd, intel_sysfs_attr_id id, int gt)
-{
-	assert(id < RPS_MAX_ATTR);
-	assert(gt < sizeof(intel_sysfs_attr_name) - 1);
-
-	return !faccessat(sysfs_dirfd, "gt", O_RDONLY, 0) ?
-		intel_sysfs_attr_name[gt + 1][id] :
-		intel_sysfs_attr_name[0][id];
-}
 
 static void process_hwconfig(void *data, uint32_t len,
 			     struct drm_i915_query_topology_info *topinfo)
@@ -571,33 +476,6 @@ static void query_hwconfig(int fd, struct drm_i915_query_topology_info *topinfo)
 	free(hwconfig);
 }
 
-static void validate_hwconfig(int drm_fd, struct drm_i915_query_topology_info *topinfo)
-{
-	struct drm_i915_query_topology_info i915_topinfo;
-
-	/*
-	 * Validate topinfo against known fixed fields for different platforms
-	 * See fill_topology_info() and intel_sseu_set_info() in i915
-	 */
-	i915_topinfo.max_slices = 1;			/* always 1 */
-	if (IS_PONTEVECCHIO(xe_dev_id(drm_fd))) {
-		i915_topinfo.max_subslices = 64;
-		i915_topinfo.max_eus_per_subslice = 8;
-	} else if (intel_graphics_ver(xe_dev_id(drm_fd)) >= IP_VER(12, 50)) {
-		i915_topinfo.max_subslices = 32;
-		i915_topinfo.max_eus_per_subslice = 16;
-	} else if (intel_graphics_ver(xe_dev_id(drm_fd)) >= IP_VER(12, 0)) {
-		i915_topinfo.max_subslices = 6;
-		i915_topinfo.max_eus_per_subslice = 16;
-	} else {
-		igt_assert(0);
-	}
-
-	igt_assert_eq(topinfo->max_slices, i915_topinfo.max_slices);
-	igt_assert_eq(topinfo->max_subslices, i915_topinfo.max_subslices);
-	igt_assert_eq(topinfo->max_eus_per_subslice, i915_topinfo.max_eus_per_subslice);
-}
-
 struct drm_i915_query_topology_info *xe_fill_i915_topology_info(int drm_fd)
 {
 	struct drm_i915_query_topology_info i915_topinfo = {};
@@ -613,8 +491,6 @@ struct drm_i915_query_topology_info *xe_fill_i915_topology_info(int drm_fd)
 	};
 
 	query_hwconfig(drm_fd, &i915_topinfo);
-	if (0)
-		validate_hwconfig(drm_fd, &i915_topinfo);
 
 	i915_topinfo.subslice_offset = 1;		/* always 1 */
 	i915_topinfo.subslice_stride = DIV_ROUND_UP(i915_topinfo.max_subslices, 8);
@@ -717,10 +593,8 @@ xe_perf_for_fd(int drm_fd, int gt)
 	if (!read_sysfs(sysfs_dir_fd, path_min, &gt_min_freq) ||
 	    !read_sysfs(sysfs_dir_fd, path_max, &gt_max_freq)) {
 		igt_warn("Unable to read freqs from sysfs\n");
-		if (!xe_relax_checks(drm_fd)) {
-			close(sysfs_dir_fd);
-			return NULL;
-		}
+		close(sysfs_dir_fd);
+		return NULL;
 	}
 	close(sysfs_dir_fd);
 
@@ -750,53 +624,10 @@ xe_perf_for_fd(int drm_fd, int gt)
 struct intel_perf *
 intel_perf_for_fd(int drm_fd, int gt)
 {
-	uint32_t device_id;
-	uint32_t device_revision;
-	uint32_t timestamp_frequency;
-	uint64_t gt_min_freq;
-	uint64_t gt_max_freq;
-	struct drm_i915_query_topology_info *topology;
-	struct intel_perf *ret;
-	int sysfs_dir_fd = open_master_sysfs_dir(drm_fd);
-
-	if (is_xe_device(drm_fd))
-		return xe_perf_for_fd(drm_fd, gt);
-
-	if (sysfs_dir_fd < 0)
+	if (!is_xe_device(drm_fd))
 		return NULL;
 
-#define read_sysfs_rps(fd, id, value) \
-	read_sysfs(fd, intel_sysfs_attr_id_to_name(fd, id, gt), value)
-
-	if (!read_sysfs_rps(sysfs_dir_fd, RPS_MIN_FREQ_MHZ, &gt_min_freq) ||
-	    !read_sysfs_rps(sysfs_dir_fd, RPS_MAX_FREQ_MHZ, &gt_max_freq)) {
-		close(sysfs_dir_fd);
-		return NULL;
-	}
-	close(sysfs_dir_fd);
-
-	if (getparam(drm_fd, I915_PARAM_CHIPSET_ID, &device_id) ||
-	    getparam(drm_fd, I915_PARAM_REVISION, &device_revision))
-		return NULL;
-
-	/* if OA_TIMESTAMP_FREQUENCY is not supported, fall back to CS_TIMESTAMP_FREQUENCY */
-	if (getparam(drm_fd, I915_PARAM_OA_TIMESTAMP_FREQUENCY, &timestamp_frequency) &&
-	    getparam(drm_fd, I915_PARAM_CS_TIMESTAMP_FREQUENCY, &timestamp_frequency))
-		return NULL;
-
-	topology = query_topology(drm_fd);
-	if (!topology)
-		return NULL;
-
-	ret = intel_perf_for_devinfo(device_id,
-				     device_revision,
-				     timestamp_frequency,
-				     gt_min_freq * 1000000,
-				     gt_max_freq * 1000000,
-				     topology);
-	free(topology);
-
-	return ret;
+	return xe_perf_for_fd(drm_fd, gt);
 }
 
 void
@@ -951,267 +782,3 @@ intel_perf_load_perf_configs(struct intel_perf *perf, int drm_fd)
 		load_metric_set_config(metric_set, drm_fd);
 	}
 }
-
-#if 0
-static void
-accumulate_uint32(const uint32_t *report0,
-                  const uint32_t *report1,
-                  uint64_t *deltas)
-{
-	*deltas += (uint32_t)(*report1 - *report0);
-}
-
-static void
-accumulate_uint40(int a_index,
-                  const uint32_t *report0,
-                  const uint32_t *report1,
-                  uint64_t *deltas)
-{
-	const uint8_t *high_bytes0 = (uint8_t *)(report0 + 40);
-	const uint8_t *high_bytes1 = (uint8_t *)(report1 + 40);
-	uint64_t high0 = (uint64_t)(high_bytes0[a_index]) << 32;
-	uint64_t high1 = (uint64_t)(high_bytes1[a_index]) << 32;
-	uint64_t value0 = report0[a_index + 4] | high0;
-	uint64_t value1 = report1[a_index + 4] | high1;
-	uint64_t delta;
-
-	if (value0 > value1)
-		delta = (1ULL << 40) + value1 - value0;
-	else
-		delta = value1 - value0;
-
-	*deltas += delta;
-}
-
-void intel_perf_accumulate_reports(struct intel_perf_accumulator *acc,
-				   const struct intel_perf *perf,
-				   const struct intel_perf_metric_set *metric_set,
-				   const struct drm_xe_oa_record_header *record0,
-				   const struct drm_xe_oa_record_header *record1)
-{
-	const uint32_t *start = (const uint32_t *)(record0 + 1);
-	const uint32_t *end = (const uint32_t *)(record1 + 1);
-	uint64_t *deltas = acc->deltas;
-	int idx = 0;
-	int i;
-
-	memset(acc, 0, sizeof(*acc));
-
-	switch (metric_set->perf_oa_format) {
-	case I915_OA_FORMAT_A24u40_A14u32_B8_C8:
-		/* timestamp */
-		if (perf->devinfo.oa_timestamp_shift >= 0)
-			deltas[idx++] += (end[1] - start[1]) << perf->devinfo.oa_timestamp_shift;
-		else
-			deltas[idx++] += (end[1] - start[1]) >> (-perf->devinfo.oa_timestamp_shift);
-		accumulate_uint32(start + 3, end + 3, deltas + idx++); /* clock */
-
-		/* 4x 32bit A0-3 counters... */
-		for (i = 0; i < 4; i++)
-			accumulate_uint32(start + 4 + i, end + 4 + i, deltas + idx++);
-
-		/* 20x 40bit A4-23 counters... */
-		for (i = 0; i < 20; i++)
-			accumulate_uint40(i + 4, start, end, deltas + idx++);
-
-		/* 4x 32bit A24-27 counters... */
-		for (i = 0; i < 4; i++)
-			accumulate_uint32(start + 28 + i, end + 28 + i, deltas + idx++);
-
-		/* 4x 40bit A28-31 counters... */
-		for (i = 0; i < 4; i++)
-			accumulate_uint40(i + 28, start, end, deltas + idx++);
-
-		/* 5x 32bit A32-36 counters... */
-		for (i = 0; i < 5; i++)
-			accumulate_uint32(start + 36 + i, end + 36 + i, deltas + idx++);
-
-		/* 1x 32bit A37 counter... */
-		accumulate_uint32(start + 46, end + 46, deltas + idx++);
-
-		/* 8x 32bit B counters + 8x 32bit C counters... */
-		for (i = 0; i < 16; i++)
-			accumulate_uint32(start + 48 + i, end + 48 + i, deltas + idx++);
-		break;
-
-	case I915_OAR_FORMAT_A32u40_A4u32_B8_C8:
-	case I915_OA_FORMAT_A32u40_A4u32_B8_C8:
-		if (perf->devinfo.oa_timestamp_shift >= 0)
-			deltas[idx++] += (end[1] - start[1]) << perf->devinfo.oa_timestamp_shift;
-		else
-			deltas[idx++] += (end[1] - start[1]) >> (-perf->devinfo.oa_timestamp_shift);
-		accumulate_uint32(start + 3, end + 3, deltas + idx++); /* clock */
-
-		/* 32x 40bit A counters... */
-		for (i = 0; i < 32; i++)
-			accumulate_uint40(i, start, end, deltas + idx++);
-
-		/* 4x 32bit A counters... */
-		for (i = 0; i < 4; i++)
-			accumulate_uint32(start + 36 + i, end + 36 + i, deltas + idx++);
-
-		/* 8x 32bit B counters + 8x 32bit C counters... */
-		for (i = 0; i < 16; i++)
-			accumulate_uint32(start + 48 + i, end + 48 + i, deltas + idx++);
-		break;
-
-	case I915_OA_FORMAT_A45_B8_C8:
-		/* timestamp */
-		if (perf->devinfo.oa_timestamp_shift >= 0)
-			deltas[0] += (end[1] - start[1]) << perf->devinfo.oa_timestamp_shift;
-		else
-			deltas[0] += (end[1] - start[1]) >> (-perf->devinfo.oa_timestamp_shift);
-
-		for (i = 0; i < 61; i++)
-			accumulate_uint32(start + 3 + i, end + 3 + i, deltas + 1 + i);
-		break;
-
-	case I915_OAM_FORMAT_MPEC8u32_B8_C8: {
-		const uint64_t *start64 = (const uint64_t *)(record0 + 1);
-		const uint64_t *end64 = (const uint64_t *)(record1 + 1);
-
-		/* 64 bit timestamp */
-		if (perf->devinfo.oa_timestamp_shift >= 0)
-			deltas[idx++] += (end64[1] - start64[1]) << perf->devinfo.oa_timestamp_shift;
-		else
-			deltas[idx++] += (end64[1] - start64[1]) >> (-perf->devinfo.oa_timestamp_shift);
-
-		/* 64 bit clock */
-		deltas[idx++] = end64[3] - start64[3];
-
-		/* 8x 32bit MPEC counters */
-		for (i = 0; i < 8; i++)
-			accumulate_uint32(start + 8 + i, end + 8 + i, deltas + idx++);
-
-		/* 8x 32bit B counters */
-		for (i = 0; i < 8; i++)
-			accumulate_uint32(start + 16 + i, end + 16 + i, deltas + idx++);
-
-		/* 8x 32bit C counters */
-		for (i = 0; i < 8; i++)
-			accumulate_uint32(start + 24 + i, end + 24 + i, deltas + idx++);
-
-		break;
-		}
-	default:
-		assert(0);
-	}
-
-}
-
-uint64_t intel_perf_read_record_timestamp(const struct intel_perf *perf,
-					  const struct intel_perf_metric_set *metric_set,
-					  const struct drm_xe_oa_record_header *record)
-{
-       const uint32_t *report32 = (const uint32_t *)(record + 1);
-       const uint64_t *report64 = (const uint64_t *)(record + 1);
-       uint64_t ts;
-
-       switch (metric_set->perf_oa_format) {
-       case I915_OA_FORMAT_A24u40_A14u32_B8_C8:
-       case I915_OA_FORMAT_A32u40_A4u32_B8_C8:
-       case I915_OA_FORMAT_A45_B8_C8:
-               ts = report32[1];
-               break;
-
-       case I915_OAM_FORMAT_MPEC8u32_B8_C8:
-               ts = report64[1];
-               break;
-
-       default:
-               assert(0);
-       }
-
-       if (perf->devinfo.oa_timestamp_shift >= 0)
-	       ts <<= perf->devinfo.oa_timestamp_shift;
-       else
-	       ts >>= -perf->devinfo.oa_timestamp_shift;
-
-       return ts;
-}
-
-uint64_t intel_perf_read_record_timestamp_raw(const struct intel_perf *perf,
-					  const struct intel_perf_metric_set *metric_set,
-					  const struct drm_xe_oa_record_header *record)
-{
-       const uint32_t *report32 = (const uint32_t *)(record + 1);
-       const uint64_t *report64 = (const uint64_t *)(record + 1);
-       uint64_t ts;
-
-       switch (metric_set->perf_oa_format) {
-       case I915_OA_FORMAT_A24u40_A14u32_B8_C8:
-       case I915_OA_FORMAT_A32u40_A4u32_B8_C8:
-       case I915_OA_FORMAT_A45_B8_C8:
-               ts = report32[1];
-               break;
-
-       case I915_OAM_FORMAT_MPEC8u32_B8_C8:
-               ts = report64[1];
-               break;
-
-       default:
-               assert(0);
-       }
-
-       if (perf->devinfo.oa_timestamp_shift >= 0)
-	       ts <<= perf->devinfo.oa_timestamp_shift;
-       else
-	       ts >>= -perf->devinfo.oa_timestamp_shift;
-
-       return ts;
-}
-
-const char *intel_perf_read_report_reason(const struct intel_perf *perf,
-					  const struct drm_xe_oa_record_header *record)
-{
-	const uint32_t *report = (const uint32_t *) (record + 1);
-
-	/* Not really documented on Gfx7/7.5*/
-	if (perf->devinfo.graphics_ver < 8)
-		return "timer";
-
-	/* Gfx8-11 */
-	if (perf->devinfo.graphics_ver < 12) {
-		uint32_t reason = report[0] >> 19;
-		if (reason & (1u << 0))
-			return "timer";
-		if (reason & (1u << 1))
-			return "trigger1";
-		if (reason & (1u << 2))
-			return "trigger2";
-		if (reason & (1u << 3))
-			return "context-switch";
-		if (reason & (1u << 4))
-			return "go-transition";
-
-		if (perf->devinfo.graphics_ver >= 9 &&
-		    reason & (1u << 5))
-			return "clock-ratio-change";
-
-		return "unknown";
-	}
-
-	/* Gfx12 */
-	if (perf->devinfo.graphics_ver <= 12) {
-		uint32_t reason = report[0] >> 19;
-		if (reason & (1u << 0))
-			return "timer";
-		if (reason & (1u << 1))
-			return "trigger1";
-		if (reason & (1u << 2))
-			return "trigger2";
-		if (reason & (1u << 3))
-			return "context-switch";
-		if (reason & (1u << 4))
-			return "go-transition";
-		if (reason & (1u << 5))
-			return "clock-ratio-change";
-		if (reason & (1u << 6))
-			return "mmio-trigger";
-
-		return "unknown";
-	}
-
-	return "unknown";
-}
-#endif
