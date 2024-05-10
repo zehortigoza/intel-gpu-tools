zehortigoza/intel-gpu-tools

commit cbe11c2939f807a9bcaa4575c268fa949fa418df

lib/i915/perf: Add support for select MPEC formats
MTL OAM xml files have been updated to specify the OA format used to
interpret the counter. The format supported for OAM is
I915_OAM_FORMAT_MPEC8u32_B8_C8, hence add support in perf library for
I915_OAM_FORMAT_MPEC8u32_B8_C8 format.

Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
Reviewed-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

lib/i915/perf.c
@@ -915,6 +915,34 @@ void intel_perf_accumulate_reports(struct intel_perf_accumulator *acc,
 		for (i = 0; i < 61; i++)
 			accumulate_uint32(start + 3 + i, end + 3 + i, deltas + 1 + i);
 		break;
+
+	case I915_OAM_FORMAT_MPEC8u32_B8_C8: {
+		const uint64_t *start64 = (const uint64_t *)(record0 + 1);
+		const uint64_t *end64 = (const uint64_t *)(record1 + 1);
+
+		/* 64 bit timestamp */
+		if (perf->devinfo.oa_timestamp_shift >= 0)
+			deltas[idx++] += (end64[1] - start64[1]) << perf->devinfo.oa_timestamp_shift;
+		else
+			deltas[idx++] += (end64[1] - start64[1]) >> (-perf->devinfo.oa_timestamp_shift);
+
+		/* 64 bit clock */
+		deltas[idx++] = end64[3] - start64[3];
+
+		/* 8x 32bit MPEC counters */
+		for (i = 0; i < 8; i++)
+			accumulate_uint32(start + 8 + i, end + 8 + i, deltas + idx++);
+
+		/* 8x 32bit B counters */
+		for (i = 0; i < 8; i++)
+			accumulate_uint32(start + 16 + i, end + 16 + i, deltas + idx++);
+
+		/* 8x 32bit C counters */
+		for (i = 0; i < 8; i++)
+			accumulate_uint32(start + 24 + i, end + 24 + i, deltas + idx++);
+
+		break;
+		}
 	default:
 		assert(0);
 	}
@@ -926,6 +954,7 @@ uint64_t intel_perf_read_record_timestamp(const struct intel_perf *perf,
 					  const struct drm_i915_perf_record_header *record)
 {
        const uint32_t *report32 = (const uint32_t *)(record + 1);
+       const uint64_t *report64 = (const uint64_t *)(record + 1);
        uint64_t ts;
 
        switch (metric_set->perf_oa_format) {
@@ -933,6 +962,10 @@ uint64_t intel_perf_read_record_timestamp(const struct intel_perf *perf,
        case I915_OA_FORMAT_A32u40_A4u32_B8_C8:
        case I915_OA_FORMAT_A45_B8_C8:
                ts = report32[1];
+               break;
+
+       case I915_OAM_FORMAT_MPEC8u32_B8_C8:
+               ts = report64[1];
                break;
 
        default:
@@ -952,6 +985,7 @@ uint64_t intel_perf_read_record_timestamp_raw(const struct intel_perf *perf,
 					  const struct drm_i915_perf_record_header *record)
 {
        const uint32_t *report32 = (const uint32_t *)(record + 1);
+       const uint64_t *report64 = (const uint64_t *)(record + 1);
        uint64_t ts;
 
        switch (metric_set->perf_oa_format) {
@@ -959,6 +993,10 @@ uint64_t intel_perf_read_record_timestamp_raw(const struct intel_perf *perf,
        case I915_OA_FORMAT_A32u40_A4u32_B8_C8:
        case I915_OA_FORMAT_A45_B8_C8:
                ts = report32[1];
+               break;
+
+       case I915_OAM_FORMAT_MPEC8u32_B8_C8:
+               ts = report64[1];
                break;
 
        default:

lib/i915/perf_data_reader.c
@@ -58,11 +58,15 @@ oa_report_ctx_is_valid(const struct intel_perf_devinfo *devinfo,
 }
 
 static uint32_t
-oa_report_ctx_id(const struct intel_perf_devinfo *devinfo, const uint8_t *report)
+oa_report_ctx_id(struct intel_perf_data_reader *reader, const uint8_t *report)
 {
-	if (!oa_report_ctx_is_valid(devinfo, report))
+	if (!oa_report_ctx_is_valid(&reader->devinfo, report))
 		return 0xffffffff;
-	return ((const uint32_t *) report)[2];
+
+	if (reader->metric_set->perf_oa_format == I915_OAM_FORMAT_MPEC8u32_B8_C8)
+		return ((const uint32_t *) report)[4];
+	else
+		return ((const uint32_t *) report)[2];
 }
 
 static void
@@ -289,8 +293,8 @@ generate_cpu_events(struct intel_perf_data_reader *reader)
 		start_report = (const uint8_t *) (last_header + 1);
 		end_report = (const uint8_t *) (current_header + 1);
 
-		last_ctx_id = oa_report_ctx_id(&reader->devinfo, start_report);
-		current_ctx_id = oa_report_ctx_id(&reader->devinfo, end_report);
+		last_ctx_id = oa_report_ctx_id(reader, start_report);
+		current_ctx_id = oa_report_ctx_id(reader, end_report);
 
 		gpu_ts_start = intel_perf_read_record_timestamp(reader->perf,
 								reader->metric_set,

commit 86712f2effc8ba6690c7e165ad63904416763b75

lib/i915/perf: Accumulate clocks for MPEC format too
Clocks/gpu_ticks should also be accumulated in intel_perf_accumulate_reports.

Fixes: cbe11c2939f8 ("lib/i915/perf: Add support for select MPEC formats")
Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>
Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
Reviewed-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>

lib/i915/perf.c
@@ -928,7 +928,7 @@ void intel_perf_accumulate_reports(struct intel_perf_accumulator *acc,
 			deltas[idx++] += (end64[1] - start64[1]) >> (-perf->devinfo.oa_timestamp_shift);
 
 		/* 64 bit clock */
-		deltas[idx++] = end64[3] - start64[3];
+		deltas[idx++] += end64[3] - start64[3];
 
 		/* 8x 32bit MPEC counters */
 		for (i = 0; i < 8; i++)